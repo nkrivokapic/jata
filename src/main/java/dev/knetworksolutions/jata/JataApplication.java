d45af748d42dc53eb918b05761d4da3dcbfaf8b7
Nemanja Krivokapic <krivokapic.nemanja@gmail.com>
2021-12-11 18:50:47 +0100

    Added Spring Boot initializer files for empty SpringBoot project

---
+++ src/main/java/dev/knetworksolutions/jata/JataApplication.java
@@ -0,0 +1,13 @@
+package dev.knetworksolutions.jata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/knetworksolutions/jata/JataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.knetworksolutions.jata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
